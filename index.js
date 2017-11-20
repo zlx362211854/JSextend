d7f68d51888a1e1043cb366c95ea0aab8354d7e1
zlx <zlx@omk.io>
2017-11-20 14:05:31 +0800

    unique

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//String
+
+// 检测字符串中是否包含指定子串
 String.prototype.find = function (str) {
-    //  检测字符串中是否包含指定子串
     var originStr = this.valueOf();
     if (str !== '') {
         var exp = new RegExp(""+str+"", "g");
@@ -8,3 +10,18 @@ String.prototype.find = function (str) {
     return false;
 };
 console.log("abcd".find('a'));
+
+// Array
+
+// 移除数组中重复的元素
+Array.prototype.unique = function () {
+    var arr = [], i, len = this.length;
+    if (len < 2) return this;
+    for (i = 0; i < len; i++) {
+        if (arr.indexOf(this[i]) === -1) {
+            arr.push(this[i]);
+        }
+    }
+    return arr;
+};
+console.log([1,2,2,3].unique());
